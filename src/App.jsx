0ca54c9083380c3fb514c4966052a0d7f01b95bd
MyroslavShymon <myroslavshymon@gmail.com>
2021-08-11 09:55:56 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,85 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import "./App.css";
 import List from "./List";
-import ListFunctional from "./ListFunctional";
 
 function App() {
-  const [visible, setVisible] = useState(true);
-  const [visibleListFunctional, setVisibleListFunctional] = useState(true);
+  const [numbers, setNumbers] = useState([
+    { id: 1, number: 1 },
+    { id: 2, number: 2 },
+    { id: 3, number: 3 },
+    { id: 4, number: 4 },
+    { id: 5, number: 5 },
+    { id: 6, number: 6 },
+    { id: 7, number: 7 },
+  ]);
+  const ulRef = useRef();
+  const timerRef = useRef();
+  const [timerState, setTimerState] = useState();
 
-  function toggleVisible() {
-    setVisible((visible) => !visible);
-  }
-  function toggleVisibleListFunctional() {
-    setVisibleListFunctional((visible) => !visible);
-  }
+  const addRandomNumber = () => {
+    const randNumber = Math.round(Math.random() * 10);
+    setNumbers((prev) => [
+      ...prev,
+      { id: prev.length + 1, number: randNumber },
+    ]);
+  };
+
+  const startTimer = () => {
+    console.log("startTimer");
+    // setTimerState(setInterval(addRandomNumber, 1000));
+    timerRef.current = setInterval(addRandomNumber, 1000);
+  };
+
+  const stopTimer = () => {
+    console.log("stopTimer");
+    // clearInterval(timerState);
+    clearInterval(timerRef.current);
+  };
+
+  const handleMouseMove = () => {
+    console.log("Handle MouseMove!");
+  };
+
+  const removeListener = () => {
+    ulRef.current.removeEventListener("mousemove", handleMouseMove);
+  };
+
+  useEffect(() => {
+    if (ulRef && ulRef.current) {
+      console.log(ulRef.current);
+      ulRef.current.addEventListener("mousemove", handleMouseMove);
+      return () => removeListener();
+    }
+  });
+
+  useEffect(() => {
+    console.log("component did update or mount");
+  });
 
   return (
     <div className="App">
-      {visible && <List />}
-      <button onClick={toggleVisible}>Change visible</button>
-
-      {visibleListFunctional && <ListFunctional />}
-      <button onClick={toggleVisibleListFunctional}>
-        Change visible of functional component
-      </button>
+      <div>
+        <ul ref={ulRef}>
+          {numbers.map((number) => (
+            <li key={number.id}>{number.number}</li>
+          ))}
+        </ul>
+        <button onClick={addRandomNumber}>Add new random number</button>
+        <button onClick={removeListener}>Remove mousemove listener</button>
+        <button onClick={startTimer}>Start add random numbers</button>
+        <button onClick={stopTimer}>Stop add random numbers</button>
+        <button onClick={() => setTimerState(1)}>
+          Change button state{timerState}
+        </button>
+        <button
+          onClick={() => {
+            timerRef.current = 1;
+          }}
+        >
+          Change button state{timerState}
+        </button>
+      </div>
     </div>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,60 +1,16 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-class List extends React.Component {
-  state = {
-    numbers: [
-      { id: 1, number: 1 },
-      { id: 2, number: 2 },
-      { id: 3, number: 3 },
-    ],
-  };
-
-  addRandomNumber = () => {
-    const randomNumber = Math.round(Math.random() * 10);
-    this.setState({
-      numbers: [
-        ...this.state.numbers,
-        {
-          id: this.state.numbers.length + 1,
-          number: randomNumber,
-        },
-      ],
-    });
-  };
-
-  componentDidMount() {
-    console.log("component did mount");
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("component did update", {
-      prevProps,
-      prevState,
-      currentProps: this.props,
-      currentState: this.state,
-    });
-    if (this.state.numbers.length !== prevState.numbers.length) {
-      console.log("List of numbers update!");
-    }
-  }
-
-  componentWillUnmount() {
-    console.log("Component is un mount");
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>Class component</h2>
-        <ul>
-          {this.state.numbers.map((number) => (
-            <li key={number.id}>{number.number}</li>
-          ))}
-        </ul>
-        <button onClick={this.addRandomNumber}>Add new random number</button>
-      </div>
-    );
-  }
-}
+const List = () => {
+  //   return (
+  //     <div>
+  //       <ul ref={ulRef}>
+  //         {numbers.map((number) => (
+  //           <li key={number.id}>{number.number}</li>
+  //         ))}
+  //       </ul>
+  //       <button onClick={addRandomNumber}>Add new random number</button>
+  //     </div>
+  //   );
+};
 
 export default List;
